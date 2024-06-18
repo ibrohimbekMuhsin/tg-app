7279736813a765335c2180989eada2c5a2273008
ibrohimbek <ibrohimbek4562@gmail.com>
2024-06-18 16:49:16 +0500

    second

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { fileURLToPath, URL } from 'node:url'
 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  publicPath: process.env.NODE_ENV === "production" ? "/https://ibrohimbekmuhsin.github.io/tg-app/" : "/",
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-});
+})
