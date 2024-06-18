202efb9e52b7e782bfd040f3eb85f466ba0c6fb9
ibrohimbek <ibrohimbek4562@gmail.com>
2024-06-18 16:57:20 +0500

    second

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/tg-app/" : "/",
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./tg-app/src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
